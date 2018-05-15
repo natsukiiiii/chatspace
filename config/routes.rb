48c0f4203a0f44e47610115da2327549c4ea17bb
natsukiiiii <w.natsuki.3@gmail.com>
2018-05-15 15:22:25 +0900

    Group edit function root_path
    Root path change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
     get 'chatspace' => 'messages_controller#index'
-    # root 'groups#index'
-      root 'messages#index'
+    root 'groups#index'
+      # root 'messages#index'
     resources :users, only: [:edit, :update]
     resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages
