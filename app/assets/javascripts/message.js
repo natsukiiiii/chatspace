84d27fe26c65a08ae93cef21f545fffb0994b960
natsukiiiii <w.natsuki.3@gmail.com>
2018-05-23 16:56:10 +0900

    プルリク　修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,56 +1,44 @@
 $(document).on("turbolinks:load", function(){
-      function buildHTML(message){
-        if (message.image.url) {
-          var img = `<img src="${message.image.url}">`
-        } else {
-          var img = ""
+  function buildHTML(message){
+    message.image.url ? img = `<img src="${message.image.url}">` : img = ""
+      var html = `<div class="message">
+                   <div class="upper-message">
+                   <div class="upper-message__user-name">
+                    ${message.name}
+                   </div>
+                   <div class ="upper-message__date">
+                   ${message.created_at}
+                   </div>
+                   </div>
+                   <p class="lower-meesage">
+                        ${message.content}
+                  </p>
+                  <div class="lower-message__image">                    ${img}
+                  </div>
+                  </div>`
+    return html;
         }
-          var html = `<div class="message">
-                       <div class="upper-message">
-                       <div class="upper-message__user-name">
-                        ${message.name}
-                       </div>
-                       <div class ="upper-message__date">
-                       ${message.created_at}
-                       </div>
-                       </div>
-                       <p class="lower-meesage">
-                            ${message.content}
-                      </p>
-                      <div class="lower-message__image">                    ${img}
-                      </div>
-                      </div>`
-              return html;
-            }
-  $('.new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(message){
-      console.log(message);
-
-
-      var html = buildHTML(message);
-      $('.messages').append(html)
-      $(".lower-message__content").val("")
-      $('#new_message')[0].reset()
-
-      // $('.lower-meesage').click(function() {
-
-      $('.chatpage__talk-page').animate({scrollTop: $('.chatpage__talk-page')[0].scrollHeight}, 'fast')
-    // })
-    })
-    .fail(function(){
-      alert('error');
-    })
-    return false;
+$('.new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
+  .done(function(message){
+    var html = buildHTML(message);
+    $('.messages').append(html)
+    $('#new_message')[0].reset()
+    $('.chatpage__talk-page').animate({scrollTop: $('.chatpage__talk-page')[0].scrollHeight}, 'fast')
+  })
+  .fail(function(){
+    alert('error');
+  })
+  return false;
+})
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,6 +1,4 @@
-json.group_id @message.group_id
 json.created_at @message.created_at
 json.name @message.user.name
 json.content @message.content
 json.image @message.image
-json.user_id @message.user_id
