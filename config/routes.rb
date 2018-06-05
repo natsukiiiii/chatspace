6ce5f58a6e456bb852a79e4ac040072f3d9e841e
natsukiiiii <w.natsuki.3@gmail.com>
2018-06-05 20:35:02 +0900

    bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-    get 'chatspace' => 'messages_controller#index'
-    root 'groups#index'
-    resources :users, only: [:index, :edit, :update]
-    resources :groups, only: [:index, :new, :create, :edit, :update] do
+  get 'chatspace' => 'messages_controller#index'
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages
-
-
-end
+  end
 end
